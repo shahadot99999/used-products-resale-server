894a607661fb43d97232ab62d3ffe0a0eb6f13af
shahadot99999 <shahadot99999@gmail.com>
2022-11-26 23:12:49 +0600

    apply in get method

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ async function run(){
         const appointmentOptionCollection1 = client.db('resaleproduct').collection('xiaomiservices');
         const appointmentOptionCollection2 = client.db('resaleproduct').collection('oneplusservice');
 
+
+         app.get('/bookings', async(req, res)=>{
+            const email = req.query.email;
+            const query = {email: email};
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+         })
+
         app.post('/bookings', async(req, res)=>{
             const booking = req.body
             //console.log(booking);
